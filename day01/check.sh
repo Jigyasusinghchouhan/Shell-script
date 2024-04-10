65e4f461bb02973099c0e3e695bd51e77dc6bf0f
Jigyasusinghchouhan <jigyashu2001@gmail.com>
2024-04-10 13:18:36 +0530

    Message describing the changes made

---
+++ day01/check.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This script prints out messages.
+
+# Print "Hello" message
+echo "Hello"
+
+# Print "DevOps" message
+echo "DevOps"
+
+# Print "Python" message
+echo "Python"
+
--- day01/hello.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This is script for TWS
-
-echo "Hello"
-
-echo "DevOps"
-
-echo "Python"
